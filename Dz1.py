58c60ad8289407cc136095b71f1c6385d512cb89
Sorokina23 <realpolosa@gmail.com>
2024-10-05 14:48:46 +0300

    Add files via upload

---
+++ Dz1.py
@@ -0,0 +1,5 @@
+print("Hellow world")
+print((9**0.5)*5) #"1st program"
+print(9.99>9.98 and 1000 != 1000.1 ) #"2nd program"
+print(2+2>2*(2+2)) #"3rd program"
+print(int((123.456*10)-1194.56)//10)#"4th program"
